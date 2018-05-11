afc8245d4670158f330486c027369072e6ff7b23
PablenkoB <pavlenkobohdan@gmail.com>
2018-05-11 12:39:40 +0300

    add formatter

--- backend/src/main/java/ua/com/mycompany/config/WebConfiguration.java
+++ backend/src/main/java/ua/com/mycompany/config/WebConfiguration.java
@@ -1,9 +1,11 @@
 package ua.com.mycompany.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import ua.com.mycompany.formater.BoardFormatter;
 
 /**
  * Creator: Pavlenko Bohdan
@@ -13,6 +15,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 @Configuration
 @EnableWebMvc
 public class WebConfiguration extends WebMvcConfigurerAdapter {
+    @Override
+    public void addFormatters(FormatterRegistry registry) {
+        BoardFormatter boardFormatter = new BoardFormatter();
+        registry.addFormatter(boardFormatter);
+        super.addFormatters(registry);
+    }
+
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("swagger-ui.html")
