f895dfe793fe454b3ce31608a15f7d95c1980c27
PablenkoB <pavlenkobohdan@gmail.com>
2018-05-23 17:10:37 +0300

    update controller

--- backend/src/main/java/ua/com/mycompany/controller/RootController.java
+++ backend/src/main/java/ua/com/mycompany/controller/RootController.java
@@ -1,16 +1,16 @@
 package ua.com.mycompany.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Creator: Pavlenko Bohdan
  * Date: 27.04.2018
  * Project: agile-boards
  */
-@RestController
+@Controller
 @RequestMapping("/")
 @Slf4j
 public class RootController {
